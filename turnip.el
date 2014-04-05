c7ee280562931608ab0b7759c75fe821e97b0bfc
Johann Klähn <kljohann@gmail.com>
2014-04-05 15:10:19 +0200

    initial import

---
+++ turnip.el
@@ -0,0 +1,165 @@
+;;; turnip.el --- Interacting with tmux from Emacs
+
+;; Copyright (C) 2014 Johann Klähn
+
+;; Author: Johann Klähn <kljohann@gmail.com>
+
+;; Keywords: tmux
+;; Package-Requires: ((dash "2.6.0") (s "1.9.0"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Turnip is a package to ease interacting with a tmux session from Emacs.
+;; Its scope and philosophy is inspired by Tim Pope's vim-tbone.
+
+;;; Code:
+
+(require 'dash)
+(require 's)
+
+(defvar turnip:attached-session nil)
+
+(defun turnip:session (&optional session)
+  (or session
+      turnip:attached-session
+      (user-error "No session specified or attached to")))
+
+(defun turnip:call-command (command &rest args)
+  (with-temp-buffer
+    (let ((status (apply #'call-process "tmux" nil t nil command args)))
+      (unless (and (numberp status) (zerop status))
+        (error "Failed: %s (%s)" command status))
+      (s-chomp (buffer-string)))))
+
+(defun turnip:call-command-split (command &rest args)
+  (s-lines (apply #'turnip:call-command command args)))
+
+(defun turnip:list-sessions ()
+  (turnip:call-command-split "list-sessions" "-F" "#S"))
+
+(defun turnip:list-windows (&optional session)
+  (append (turnip:call-command-split "list-windows" "-F" "#W" "-t" (turnip:session session))
+          (turnip:call-command-split "list-windows" "-F" "#S:#W" "-a")))
+
+(defun turnip:list-panes (&optional session)
+  (append (turnip:call-command-split "list-panes" "-F" "#W.#P" "-s" "-t" (turnip:session session))
+          (turnip:call-command-split "list-panes" "-F" "#S:#W.#P" "-s" "-a")
+          '("last" "top" "bottom" "left" "right"
+            "top-left" "top-right" "bottom-left" "bottom-right")))
+
+(defun turnip:list-clients ()
+  (turnip:call-command-split "list-clients" "-F" "#{client_tty}"))
+
+(defun turnip:list-buffers ()
+  (-map #'number-to-string
+        (number-sequence
+         0 (1- (length (turnip:call-command-split "list-buffers"))))))
+
+(defun turnip:list-executables ()
+  (let* ((path (turnip:call-command "show-environment" "-g" "PATH"))
+         (dirs (parse-colon-path (s-chop-prefix "PATH=" path))))
+    (->> dirs
+      (-map-when #'file-directory-p
+                 (lambda (dir)
+                   (--filter (file-executable-p (expand-file-name it dir))
+                             (directory-files dir nil nil 'nosort))))
+      -flatten
+      -uniq)))
+
+(defun turnip:parse-command-options (line)
+  (let
+      ((option-names
+        (->> line
+          (s-match-strings-all "[[|]-\\(\\w+\\)")
+          (-map #'cadr)
+          (apply #'concat)))
+       (option-arguments
+        (-when-let*
+            ((matches (s-match-strings-all
+                       "[[|]\\(-\\w\\)\\s-+\\([^]|]+\\)[]|]" line)))
+          (--map (cons (cadr it) (-last-item it)) matches))))
+    (cons option-names option-arguments)))
+
+(defun turnip:list-commands ()
+  (let ((lines (turnip:call-command-split "list-commands")))
+    (apply
+     #'append
+     (-map (lambda (line)
+             (-when-let*
+                 ((match (s-match "^\\(\\S-+\\)\\(?:\\s-+\\(.*\\)\\)?$" line))
+                  (cmd (cadr match))
+                  (rest (-last-item match)))
+               (let ((options (turnip:parse-command-options rest))
+                     (alias (cadr (s-match "(\\([^)]+\\))" rest))))
+                 (append
+                  (list (cons cmd options))
+                  (when alias
+                    (list (cons alias options))))))) lines))))
+
+(defun turnip:completion-list-for-argument (arg &optional session)
+  (cond
+   ((s-suffix? "-pane" arg) (turnip:list-panes session))
+   ((s-suffix? "-window" arg) (turnip:list-windows session))
+   ((s-suffix? "-session" arg) (turnip:list-sessions))
+   ((s-suffix? "-client" arg) (turnip:list-clients))
+   ((s-equals? "buffer-index" arg) (turnip:list-buffers))))
+
+;;;###autoload
+(defun turnip-attach ()
+  "Attach to a particular tmux session."
+  (interactive)
+  (let* ((sessions (turnip:list-sessions))
+         (choice (if (= (length sessions) 1)
+                     (car sessions)
+                   (completing-read "Session: " sessions nil t))))
+    (when (s-equals? choice "")
+      (user-error "No session name provided"))
+    (setq turnip:attached-session choice)))
+
+;;;###autoload
+(defun turnip-command ()
+  "Interactively prompts for a tmux command to execute.
+The command will be built in several steps.  First the user can choose
+the tmux command to run.  In the next prompts completion for the options
+of this command is provided.  The command will be executed once the user
+gives an empty answer."
+  (interactive)
+  (-when-let* ((commands (turnip:list-commands))
+               (command-names (-map #'car commands))
+               (choice (completing-read "tmux " command-names nil t)))
+    (let* ((options (cdr (assoc choice commands)))
+           (option-names (--map (concat "-" (list it)) (car options)))
+           (option-arguments (cdr options))
+           (arguments))
+
+      (while (not (s-equals? choice ""))
+        (setq arguments (-snoc arguments choice))
+        (setq option-names (--remove (s-equals? choice it) option-names))
+        (let ((prompt (format "tmux %s " (s-join " " arguments)))
+              (takes-argument (cdr (assoc choice option-arguments))))
+          (if (not takes-argument)
+              (setq choice (completing-read prompt option-names))
+            (setq choice "")
+            (while (s-equals? choice "")
+              (setq choice
+                    (completing-read
+                     (format "%s[%s] " prompt takes-argument)
+                     (turnip:completion-list-for-argument takes-argument)))))))
+      (apply #'turnip:call-command arguments))))
+
+(provide 'turnip)
+
+;;; turnip.el ends here
